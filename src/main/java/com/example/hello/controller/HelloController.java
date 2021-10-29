1964b7dd3fb06cd7b20df7c354c6ed06dcd00c54
王羽涵 <wangyuhan@beyondcent.com>
2021-10-29 23:18:25 +0800

    third commit

---
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YuHan Wang
+ * @date 2021/10/29 23:17
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
