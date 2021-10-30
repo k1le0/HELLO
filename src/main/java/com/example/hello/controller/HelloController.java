4720e8fc672e782e974082b00fb9fa215e79ba1a
王羽涵 <wangyuhan@beyondcent.com>
2021-10-30 13:15:18 +0800

    seventh commit

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/${id}")
+    public String show(@PathVariable("id") Integer id) {
+        return String.valueOf(id);
     }
 }
